06af7ad0c545c9decf6aa9b3530c4b42b132c1d4
osei <williamsabodunrin@gmail.com>
2023-07-24 03:43:31 -0700

    shell sort algorithm

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -10,6 +10,9 @@ void shell_sort(int *array, size_t size)
 	size_t i, j, incr = 1;
 	int temp;
 
+	if (array = NULL || size < 2)
+		return;
+
 	while (incr <= (size - 1)/9)
 		incr = (incr * 3) + 1;
 
