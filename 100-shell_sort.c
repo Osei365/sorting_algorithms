65bb571ec69adb1d40b735235f0d2ea822fcd3f4
osei <williamsabodunrin@gmail.com>
2023-07-24 03:40:58 -0700

    shell sort algorithm

---
+++ 100-shell_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+/**
+ * shell_sort - sorts using shell sort algorithm
+ * Knuth's sequence
+ * @array: array to sort
+ * @size: size of array
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t i, j, incr = 1;
+	int temp;
+
+	while (incr <= (size - 1)/9)
+		incr = (incr * 3) + 1;
+
+	while (incr > 0)
+	{
+		for (i = incr; i < size; i++)
+		{
+			temp = array[i];
+			for (j = i; j >= incr; j = j - incr)
+			{
+				if (array[j - incr] > temp)
+				{
+					array[j] = array[j - incr];
+				}
+				else
+					break;
+			}
+			array[j] = temp;
+		}
+		incr = incr / 3;
+		print_array(array, size);
+	}
+}
