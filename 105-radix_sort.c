6b9f470c00add2cbc54b293aff1d65d58a0bb505
osei <williamsabodunrin@gmail.com>
2023-07-25 10:15:18 -0700

    radix sort LSD

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -19,51 +19,63 @@ int get_max(int *array, size_t size)
 }
 
 /**
+ * countsort - sort using counting sort
+ * algorithm
+ * @array: array to be sorted
+ * @size: size of array
+ * @place: place value
+ */
+void countsort(int *array, size_t size, int place)
+{
+	int *count, *output, i;
+	size_t max, a;
+
+	max = (array[0] / place) % 10;
+	for (a = 1; a < size; a++)
+	{
+		if ((array[a] / place) % 10 > (int)max)
+			max = array[a];
+	}
+
+	output = malloc(sizeof(int) * size);
+	if (output == NULL)
+		return;
+	count = malloc(sizeof(int) * (max + 1));
+	if (count == NULL)
+	{
+		free(output);
+		return;
+	}
+	for (a = 0; a <= max; a++)
+		count[a] = 0;
+
+	for (a = 0; a < size; a++)
+		count[(array[a] / place) % 10]++;
+
+	for (a = 1; a <= 10; a++)
+		count[a] += count[a - 1];
+	for (i = size - 1; i >= 0; i--)
+		output[--count[(array[i] / place) % 10]] = array[i];
+	for (a = 0; a < size; a++)
+		array[a] = output[a];
+	free(count);
+	free(output);
+}
+/**
  * radix_sort - sort based on LSD radix sort
  * @array: array to be sorted
  * @size: size of array
  */
 void radix_sort(int *array, size_t size)
 {
-	int max = get_max(array, size);
-	int a, b, c, d, *newarray, temp, min;
+	int max = get_max(array, size), place;
 
 	if (array == NULL || size < 2)
 		return;
-	newarray = malloc(sizeof(int) * size);
-	if (newarray == NULL)
-		return;
-	for (a = 0; a < (int)size; a++)
-		newarray[a] = array[a];
 
-	for (a = 1; max / a > 0; a = a * 10)
+	for (place = 1; max / place > 0; place *= 10)
 	{
-		for (b = 0; b < (int)size; b++)
-		{
-			min = newarray[b] % 10;
-			d = b;
-			for (c = b + 1; c < (int)size; c++)
-			{
-				if (min > newarray[c] % 10)
-				{
-					min = newarray[c] % 10;
-					d = c;
-				}
-			}
-			if (d != b)
-			{
-				temp = array[d];
-				array[d] = array[b];
-				array[b] = temp;
-
-				temp = newarray[d];
-				newarray[d] = newarray[b];
-				newarray[b] = temp;
-			}
-		}
-		for (b = 0; b < (int)size; b++)
-			newarray[b] = newarray[b] / 10;
+		countsort(array, size, place);
 		print_array(array, size);
 	}
-	free(newarray);
 }
